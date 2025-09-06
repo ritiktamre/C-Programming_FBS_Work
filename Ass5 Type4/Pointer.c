8975fc8cbd76381f30e729da33c95377c531cc84
ritiktamre <ritiktamre@gmail.com>
2025-09-06 18:33:53 +0530

    Add files via upload

---
+++ Ass5 Type4/Pointer.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void factorial(int*);
+int main()
+{
+	int no1;
+	printf("enter any number:");
+	scanf("%d",&no1);
+	 factorial(&no1);
+}
+   void factorial(int* a)
+{
+   	int num,i,fact=1;
+    num=* a;
+	
+	for(i=1;i<=* a;i++)
+	{
+		fact=fact*i;
+	}
+	printf("FACTORIAL=%d",fact);
+}
